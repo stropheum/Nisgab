4edb3b5a12fd079931fc6607e3a3f0d90e61d2bc
Stropheum <stropheum060@gmail.com>
2025-11-28 16:41:30 -0500

    rename EventBus to InputEventBus in InputActionGenerator for clarity

--- Editor/Scripts/InputActionGenerator.cs
+++ Editor/Scripts/InputActionGenerator.cs
@@ -80,7 +80,7 @@ namespace Editor.NisGab
             sb.Append(CreateHeader(assetName));
             sb.AppendLine("namespace " + OutputNamespace);
             sb.AppendLine("{");
-            sb.AppendLine("\tpublic class EventBus : LazySingleton<EventBus>");
+            sb.AppendLine("\tpublic class InputEventBus : LazySingleton<EventBus>");
             sb.AppendLine("\t{");
             foreach (InputActionMap inputActionMap in inputActionMaps)
             {
